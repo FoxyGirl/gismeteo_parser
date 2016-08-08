e61144964b15efdf344e92bf60b0996d4628d795
Elena Pavlova <lenlho22@gmail.com>
2016-08-08 23:46:40 +0300

    Roma's help YQLQuery

---
+++ js/roma.js
@@ -0,0 +1,65 @@
+/* jshint browser: true */
+/* jshint devel: true */
+
+function YQLQuery(query, callback) {
+    this.query = query;
+    this.callback = callback || function(){};
+    this.fetch = function() {
+
+        if (!this.query || !this.callback) {
+            throw new Error('YQLQuery.fetch(): Parameters may be undefined');
+        }
+
+        var scriptEl = document.createElement('script'),
+            uid = 'yql' + (+new Date()),
+            encodedQuery = encodeURIComponent(this.query.toLowerCase()),
+            instance = this;
+
+        YQLQuery[uid] = function(json) {
+            instance.callback(json);
+            delete YQLQuery[uid];
+            document.body.removeChild(scriptEl);
+        };
+
+        scriptEl.src = 'http://query.yahooapis.com/v1/public/yql?q='
+                     + encodedQuery + '&format=json&callback=YQLQuery.' + uid; 
+        document.body.appendChild(scriptEl);
+
+    };
+} 
+
+  function fct() {
+    console.log("adfas");
+    var data = "",
+    url = "",
+    neededUrl = 'https://www.gismeteo.by/weather-minsk-4248/month/';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4){
+        data = xhr.responseText;
+        }
+    };
+    xhr.send();
+    console.log(data);
+    
+
+    var query = "select * from html where url='"+ neededUrl +"'";
+
+    // Define your callback:
+    var callback = function(data) {
+        //var post = data.query.results.item;
+        //alert(post.title);
+      var result = data.query.results;
+        console.log(result);
+         console.log(JSON.stringify(result));
+        document.getElementById('resultOut').innerHTML = result;
+    };
+
+    // Instantiate with the query:
+    var firstFeedItem = new YQLQuery(query, callback);
+
+    // If you're ready then go:
+    firstFeedItem.fetch(); // Go!!
+  }
