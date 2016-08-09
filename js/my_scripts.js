43ae28554f87aa7642e7d7b9bfcfc988f3c893eb
Elena Pavlova <lenlho22@gmail.com>
2016-08-09 22:07:53 +0300

    Use dot notification ad return newData

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -50,7 +50,8 @@ function YQLQuery(query, callback) {
         console.log(result);      
         // console.log(JSON.stringify(result));
 
-      parseGismeteo(result);
+      var newData = parseGismeteo(result);
+       console.log('newData = ' + JSON.stringify(newData[10]));
     };
 
     // Instantiate with the query:
@@ -67,25 +68,22 @@ function trim(str) {
 function parseGismeteo(resultJSON) {
   var result = resultJSON;
   var newData = [];
-  for (var i = 0, l = result['div'].length; i < l; i++) {
+  for (var i = 0, l = result.div.length; i < l; i++) {
    // console.log(result['div'][i]);
 
     newData[i] = {};
     if ( i < 10 ) {
-      newData[i]['date'] = trim(result['div'][i]['div'][0]['a']['content']);
+      newData[i].date = trim(result.div[i].div[0].a.content);
     } else {
-      newData[i]['date'] = trim(result['div'][i]['div'][0]['content']);
+      newData[i].date = trim(result.div[i].div[0].content);
     }
     
-    newData[i]['temp_max'] = trim(result['div'][i]['div'][2]['div'][0]['data-value']);
+    newData[i].temp_max = trim(result.div[i].div[2].div[0]['data-value']);
     
-    newData[i]['temp_min'] = trim(result['div'][i]['div'][2]['div'][1]['data-value']);
-    
-    console.log('date = ' + newData[i]['date']);
-    console.log('temp_max = ' + newData[i]['temp_max']);
-    console.log('temp_min = ' + newData[i]['temp_min']);
+    newData[i].temp_min = trim(result.div[i].div[2].div[1]['data-value']);
     
   }
   
-  console.log('newData = ' + JSON.stringify(newData[10]));
+  //console.log('newData = ' + JSON.stringify(newData[10]));
+  return newData;
 }
