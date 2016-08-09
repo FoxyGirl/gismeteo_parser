f478fdc5091357579aa9da8a15a3765cb22ed872
Elena Pavlova <lenlho22@gmail.com>
2016-08-10 00:06:42 +0300

    Create table from newData

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -6,6 +6,8 @@
   input.addEventListener('click', fct);  
 })();
 
+var oldResult = "";
+
 function YQLQuery(query, callback) {
     this.query = query;
     this.callback = callback || function(){};
@@ -32,40 +34,43 @@ function YQLQuery(query, callback) {
     };
 } 
 
-  function fct() {
-    console.log("!!!");
-    var data = "",
-        oldResult = "",
-        clickInput = this,
-        neededUrl = 'https://www.gismeteo.by/weather-minsk-4248/month/';
-    
-    clickInput.disabled = 'true';
+function fct() {
+  console.log("!!!");
+  var data = "",        
+      clickInput = this,
+      neededUrl = 'https://www.gismeteo.by/weather-minsk-4248/month/';
 
-   // var query = "select * from html where url='"+ neededUrl +"'";
-  
-    var query = "select * from html where url=\""+ neededUrl +"\" and xpath='//div[contains(@class,\"cell_content\")]'";
+  clickInput.disabled = true;
 
-    // Define your callback:
-    var callback = function(data) {
-      var result = data.query.results;
-        console.log(result);
-        if ( result === oldResult) {
-          return;
-        }
-        // console.log(JSON.stringify(result));
-          
+ // var query = "select * from html where url='"+ neededUrl +"'";
+
+  var query = "select * from html where url=\""+ neededUrl +"\" and xpath='//div[contains(@class,\"cell_content\")]'";
+
+  // Define your callback:
+  var callback = function(data) {
+    var result = data.query.results;
+      console.log(result);
+      //if result not be changed do nothing
+      if ( JSON.stringify(result) === oldResult) {
+        return;
+      }
+      oldResult = JSON.stringify(result);
+      // console.log(JSON.stringify(result));
+
       var newData = parseGismeteo(result);
        console.log('newData = ' + JSON.stringify(newData[10]));
-      
-      clickInput.disabled = 'false';
-    };
 
-    // Instantiate with the query:
-    var firstFeedItem = new YQLQuery(query, callback);
+      createTable(newData);
+      clickInput.disabled = false;
 
-    // If you're ready then go:
-    firstFeedItem.fetch(); // Go!!
-  }
+  };
+
+  // Instantiate with the query:
+  var firstFeedItem = new YQLQuery(query, callback);
+
+  // If you're ready then go:
+  firstFeedItem.fetch(); // Go!!
+}
 
 function trim(str) {
   return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
@@ -92,4 +97,48 @@ function parseGismeteo(resultJSON) {
   
   //console.log('newData = ' + JSON.stringify(newData[10]));
   return newData;
+}
+
+function createTable(newData) {
+  var out = document.getElementById('resultTable');
+  var fragment = document.createDocumentFragment();
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+  var tr = document.createElement('tr');
+  
+  //Create thead
+  var thArray = ['Date', 'Temp max', 'Temp min'];
+  
+  for ( var i = 0; i < thArray.length; i++ ) {
+    var th = document.createElement('th');
+    th.appendChild(document.createTextNode(thArray[i]));
+    tr.appendChild(th);
+  }
+  
+  thead.appendChild(tr);
+  table.appendChild(thead);
+  
+  //Create tbody
+  for ( var i = 0, l = newData.length; i < l; i++ ) {
+    var trTbody = document.createElement('tr');
+    
+    var tdDate = document.createElement('td');
+    tdDate.appendChild(document.createTextNode(newData[i].date));
+    
+    var tdTempMax = document.createElement('td');    tdTempMax.appendChild(document.createTextNode(newData[i].temp_max)); 
+    
+    var tdTempMin = document.createElement('td');    tdTempMin.appendChild(document.createTextNode(newData[i].temp_min));
+    
+    trTbody.appendChild(tdDate);
+    trTbody.appendChild(tdTempMax);
+    trTbody.appendChild(tdTempMin);    
+    
+    tbody.appendChild(trTbody);
+  }
+  
+  table.appendChild(tbody);
+  fragment.appendChild(table);
+  
+  out.appendChild(table);
 }
